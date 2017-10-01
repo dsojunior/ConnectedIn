46c04b76c893633e844e2b3af605b8fffc6d11ac
Décio Oliveira Jr <decio.oliveira@usp.br>
2017-10-01 17:49:19 -0300

    Aula 03 - A página Perfil

--- perfis/views.py
+++ perfis/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from perfis.models import Perfil
 
 def index(request):
 	return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+	perfil = Perfil()
+
+	if perfil_id =='1':
+		perfil = Perfil('Flavio Almeida', 'flavio@flavio.com.br', '777777', 'Caelum')
+	if perfil_id == '2':
+		perfil = Perfil('Romulo Henrique', 'romulo@romulo.com.br', '888888', 'Caelum')
+
+	return render(request, 'perfil.html', { "perfil" : perfil})
